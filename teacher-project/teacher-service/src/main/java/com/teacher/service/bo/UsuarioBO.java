4820d3d6e27ab88c7d181ba93fc77e290dc41bf5
Everson <everson_z@yahoo.com>
2015-12-18 03:05:07 -0200

    mybatis

--- teacher-project/teacher-service/src/main/java/com/teacher/service/bo/UsuarioBO.java
+++ teacher-project/teacher-service/src/main/java/com/teacher/service/bo/UsuarioBO.java
@@ -4,16 +4,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.teacher.common.vo.UsuarioVO;
-import com.teacher.data.mapper.UsuarioMapper;
+import com.teacher.data.mapper.*;
+
 
 @Service
 public class UsuarioBO {
 	
 	@Autowired
-	private UsuarioMapper usuarioMapper;
+	Teste teste;
+	@Autowired
+	UsuarioMapper usuarioMapper;
 	
 	public UsuarioVO login(String user, String pass) {
 		
+		teste.teste();
 		usuarioMapper.login(user, pass);
 		
 		return null;
